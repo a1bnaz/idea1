d4a953f5092ccb63acbf7293e55202848ceffb25
albert <ieatbaconhair@gmail.com>
2025-12-14 04:35:07 -0500

    initial commit

---
+++ backend/src/main/java/com/idea1/app/backend/Idea1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.idea1.app.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idea1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Idea1BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/idea1/app/backend/Idea1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idea1.app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Idea1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
